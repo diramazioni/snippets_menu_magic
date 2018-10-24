a6fa946d7ed5bb286c2e20fa15fdbea9963b4fb5
Eli Spizzichino <scrivi@gmail.com>
2018-10-24 10:59:24 +0200

    version bump

--- snippets_menu_magic/__init__.py
+++ snippets_menu_magic/__init__.py
@@ -1,12 +1,12 @@
- 
-""" Snippets menu magic 
-    Let's you create menu directly from the notebook. 
+
+""" Snippets menu magic
+    Let's you create menu directly from the notebook.
     The menu elements can be snippets of code or internal/external links
 """
-__version__ = '0.2'
+__version__ = '0.2.1'
 
 from .snippets import SnippetsMenuMagic
 
 def load_ipython_extension(ipython):
     sm = SnippetsMenuMagic(ipython)
-    ipython.register_magics(sm)    
+    ipython.register_magics(sm)
